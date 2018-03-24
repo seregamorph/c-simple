17993ad80e3c58f0f69c13265ca8bbf5276c787c
seregamorph <seregamorph@gmail.com>
2018-03-24 13:07:45 +0300

    first version

---
+++ src/main/java/com/seregamorph/c/Simple.java
@@ -0,0 +1,176 @@
+package com.seregamorph.c;
+
+import java.util.Scanner;
+
+public class Simple {
+
+    public static void main(String[] args) {
+        int n;
+        if (args.length == 0) {
+            System.out.print("Enter N (must be positive integer): ");
+            Scanner in = new Scanner(System.in);
+            n = in.nextInt();
+        } else if (args.length == 1) {
+            n = Integer.parseInt(args[0]);
+        } else {
+            printUsage();
+            return;
+        }
+        if (n <= 0) {
+            printUsage();
+            return;
+        }
+
+        int s = arraySimpleInt(n, false);
+        System.out.println(s);
+    }
+
+    private static void printUsage() {
+        System.err.println("Program expects none or single parameter N > 0");
+    }
+
+    @Deprecated // не представляет большого интереса
+    static int basicSimpleInt(int n, boolean trace) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        // i - отсчет простых чисел (до n)
+        int i = 2;
+        // m - число, которое проверяем на простоту
+        cm:
+        for (int m = 3; ; m += 2) {
+            if (m < 0) {
+                throw new IllegalStateException("int overload, last i=" + i);
+            }
+            int maxd = (int) Math.round(Math.sqrt(m));
+
+            // d - делитель
+            for (int d = 3; d <= maxd; d += 2) {
+                if (m % d == 0) {
+                    continue cm;
+                }
+            }
+            // число m простое
+            if (++i >= n) {
+                return m;
+            }
+
+            if (trace && i % 1_000_000 == 0) {
+                System.out.println(i + " " + m);
+            }
+        }
+    }
+
+    static long basicSimpleLong(int n, boolean trace) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        // i - отсчет простых чисел (до n)
+        int i = 2;
+        // m - число, которое проверяем на простоту
+        cm:
+        for (long m = 3; ; m += 2) {
+            if (m < 0) {
+                throw new IllegalStateException("long overload, last i=" + i);
+            }
+            long maxd = Math.round(Math.sqrt(m));
+
+            // d - делитель
+            for (long d = 3; d <= maxd; d += 2) {
+                if (m % d == 0) {
+                    continue cm;
+                }
+            }
+            // число m простое
+            if (++i >= n) {
+                return m;
+            }
+
+            if (trace && i % 1_000_000 == 0) {
+                System.out.println(i + " " + m);
+            }
+        }
+    }
+
+    static int arraySimpleInt(int n, boolean trace) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        int[] arr = new int[n - 3];
+        // i - индекс в массиве
+        int i = 0;
+        // m - число, которое проверяем на простоту
+        int m = 3;
+        cm:
+        for (; ; m += 2) {
+            if (m < 0) {
+                throw new IllegalStateException("int overload, last i=" + i + " and m=" + arr[i - 1]);
+            }
+
+            double sqrt = Math.sqrt(m);
+            int maxd = (int) Math.round(sqrt);
+
+            int d;
+            for (int j = 0; j < i && (d = arr[j]) <= maxd; j++) {
+                if (m % d == 0) {
+                    continue cm;
+                }
+            }
+            // m - простое
+            if (++i >= n - 2) {
+                return m;
+            }
+            arr[i - 1] = m;
+            if (trace && i % 1_000_000 == 0) {
+                System.out.println(i + " " + m);
+            }
+        }
+    }
+
+    static long arraySimpleLong(int n, boolean trace) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        long[] arr = new long[n - 3];
+        // i - индекс в массиве
+        int i = 0;
+        // m - число, которое проверяем на простоту
+        long m = 3;
+        cm:
+        for (; ; m += 2) {
+            if (m < 0) {
+                throw new IllegalStateException("int overload, last i=" + i);
+            }
+
+            double sqrt = Math.sqrt(m);
+            long maxd = Math.round(sqrt);
+
+            long d;
+            for (int j = 0; j < i && (d = arr[j]) <= maxd; j++) {
+                if (m % d == 0) {
+                    continue cm;
+                }
+            }
+            // m - простое
+            if (++i >= n - 2) {
+                return m;
+            }
+            arr[i - 1] = m;
+            if (trace && i % 1_000_000 == 0) {
+                System.out.println(i + " " + m);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/seregamorph/c/SimpleTest.java
@@ -0,0 +1,97 @@
+package com.seregamorph.c;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void testSmall() {
+        checkAll(1, 1);
+        checkAll(2, 2);
+        checkAll(3, 3);
+        checkAll(4, 5);
+        checkAll(5, 7);
+        checkAll(6, 11);
+        checkAll(7, 13);
+        checkAll(8, 17);
+        checkAll(9, 19);
+        checkAll(10, 23);
+        checkAll(11, 29);
+        checkAll(12, 31);
+        checkAll(13, 37);
+        checkAll(14, 41);
+        checkAll(15, 43);
+        checkAll(16, 47);
+        checkAll(17, 53);
+        checkAll(18, 59);
+
+        checkAll(500, 3559);
+        checkAll(501, 3571);
+    }
+
+    @Test
+    public void testBasicBig1() {
+        // 18s
+        assertEquals(27_644_437, Simple.basicSimpleInt(1_720_726, true));
+    }
+
+    @Test
+    public void testArrayBig1() {
+        // 4s
+        assertEquals(27_644_437, Simple.arraySimpleInt(1_720_726, true));
+    }
+
+    @Test
+    public void testBasicBig2() {
+        // 32s
+        assertEquals(45_561_077, Simple.basicSimpleInt(2_750_142, true));
+    }
+
+    @Test
+    public void testArrayBig2() {
+        // 8s
+        assertEquals(45_561_077, Simple.arraySimpleInt(2_750_142, true));
+    }
+
+    @Test
+    @Ignore // too long
+    public void testBasicBig3() {
+        //
+        assertEquals(373_587_839, Simple.basicSimpleInt(20_000_000, true));
+    }
+
+    @Test
+    public void testArrayBig3() {
+        // 2m36s
+        assertEquals(373_587_839, Simple.arraySimpleInt(20_000_000, true));
+    }
+
+    @Test
+    public void testArrayBig4() {
+        // 27m35s
+        assertEquals(2_038_074_739, Simple.arraySimpleInt(100_000_000, true));
+    }
+
+    @Test
+    public void testIntArrayBig5() {
+        // maximum int
+        // 30m
+        assertEquals(2147483647, Simple.arraySimpleInt(105_097_566, true));
+    }
+
+    @Test
+    public void testLongArrayBig6() {
+        // more than maximum int - long
+        // 1h30m
+        assertEquals(2147483659L, Simple.arraySimpleLong(105_097_567, true));
+    }
+
+    private static void checkAll(int n, int expected) {
+        assertEquals(expected, Simple.basicSimpleInt(n, true));
+        assertEquals(expected, Simple.basicSimpleLong(n, true));
+        assertEquals(expected, Simple.arraySimpleInt(n, true));
+    }
+}
